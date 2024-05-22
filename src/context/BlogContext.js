79331aae7af90b33de6b4d64298141ec7dcf2c35
Ravi <ravipanchalvishwakarma@gmail.com>
2024-05-22 18:02:36 +0530

    Replaced useState with useReducer hook

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogPost":
+      return [
+        ...state,
+        {
+          title: `Blog Post #${state.length + 1}`,
+          content: "Lorem ipsum is a simply dummy text",
+        },
+      ];
+
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      {
-        title: `Blog Post #${blogPosts.length + 1}`,
-        content: "Lorem ipsum is a simply dummy text",
-      },
-    ]);
+    dispatch({ type: "add_blogPost" });
   };
 
   return (
